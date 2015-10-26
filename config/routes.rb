1ce3ef410b4e33d0e8110b8d78ecb7ac92380db1
codecademy <ccuser@codecademy.com>
2015-10-26 17:34:06 -0400

    About page added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home', as: :pages
+  get 'about' => 'pages#about', as: :about
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
